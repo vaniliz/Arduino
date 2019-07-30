faabfe6a8e78f04b2dc734d1519a7622a61b3567
Vaniliz <vaniliz.14@gmail.com>
2019-07-30 13:23:16 -0500

    Modificacion de Demo1

--- Demo1.ino
+++ Demo1.ino
@@ -1,9 +1,13 @@
 void setup() {
   // put your setup code here, to run once:
-
+ Serial.begin (9600);
+ Serial.println ("Inicializando programa");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+ Serial.println("Tenego flojera de programar");
+ //while (1);
+  Serial.print(".");
+  delay(500);
 }
